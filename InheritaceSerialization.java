d90c9ebbac50399e9924d687e63cd99a4251ad82
RAVI RANJAN KUMAR <ravi115ranjan@gmail.com>
2017-08-18 12:11:04 +0530

    complete serialization w.r.t inheritance

--- InheritaceSerialization.java
+++ InheritaceSerialization.java
@@ -16,6 +16,8 @@ import java.io.Serializable;
 
 
 /* below example illustrates for case -1.
+ * 
+ * 
  * when parent class implements serializable interface but child class doesn't implements.
  * in this case,  if parent class is implementing serializable interface that fine for child class also. no need of child 
  * class to implement serializable interface. since child can contain each and every functionality of parent class except
@@ -48,8 +50,40 @@ class Child extends Parent {
 
 }
 
+/**
+ * Case -2 : when child implements serializable but parent doesn't.
+ *  
+ * in this case also serialization works fine but there is four points to be remmembred.
+ * 1 . while serialization process JVM will check whether any parent is not implementing serializable interface.
+ * 2. if it is so then JVM will ignore original value of parent's class variable and save default value.
+ * 3. while de-serialization again JVM will check whether parent is implementing serializable interface or not. if so
+ * 4. then JVM will construct an object by checking no-argument constructor (basically known as instance flow control) and
+ * 		put original value back for parent's variable in that de-serialized object.  
+ *
+ */
 
+class NewParent {
+	
+	int i = 10;
+	public NewParent() {
+		System.out.println("Parent constructor");
+	}
+	
+}
 
+class NewChild extends NewParent implements Serializable {
+	
+	int j = 20;
+	
+	public NewChild() {
+		System.out.println("Child class constructor called");
+	}
+	
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " : { i : " + i + " |  j : " + j + " }";
+	}
+}
 public class InheritaceSerialization {
 
 	/**
@@ -58,8 +92,11 @@ public class InheritaceSerialization {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Child obj1 = new Child(1, "ravi", 23, "google111");
-
+		//Child obj1 = new Child(1, "ravi", 23, "google111");
+		NewChild obj1 = new NewChild();
+		obj1.i = 999;
+		obj1.j = 888;
+		
 		//serialization takes place hare
 		FileOutputStream fOs = new FileOutputStream("abc.ser");
 		ObjectOutputStream oos = new ObjectOutputStream(fOs);
@@ -76,8 +113,8 @@ public class InheritaceSerialization {
 		Object obj = oin.readObject();
 		oin.close();
 		//check if obj belongs to your desired class or not.
-		if(obj instanceof Child ) {
-			Child pm= (Child) obj;
+		if(obj instanceof NewChild ) {
+			NewChild pm= (NewChild) obj;
 			System.out.println("*-----after deserialization-----*");
 			System.out.println(pm);
 		}
